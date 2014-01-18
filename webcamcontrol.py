e4724acd2a18c790bad1f87623aa158f3905d780
Greg Lutostanski <greg.luto@gmail.com>
2014-01-18 13:51:42 -0600

    Refactor fun.
    Comply with pep8, and make code more readable and a bit more pythonic

--- webcamcontrol.py
+++ webcamcontrol.py
@@ -1,118 +1,120 @@
 #!/usr/bin/env python2
+"""Useful script that detects movement from a v4l camera and outputs useful
+text for controlling music players.
 
+"""
 import sys
 import cv2
-import math
-import numpy as np
+import numpy
 
-#these are the main sources of information that I squeezed into this program:
-#http://www.steinm.com/blog/motion-detection-webcam-python-opencv-differential-images/
-#http://derek.simkowiak.net/motion-tracking-with-python/
-#http://stackoverflow.com/questions/3374828/how-do-i-track-motion-using-opencv-in-python
-#http://opencvpython.blogspot.com/2012/07/background-extraction-using-running.html
-#http://stackoverflow.com/questions/9013703/how-to-find-the-location-of-red-region-in-an-image-using-matlab/
+WIDTH = 320
+HEIGHT = 240
+DIFFERENCE = min(WIDTH / 2, HEIGHT / 2) * 0.80
 
-def average(a):
-    #yeah I know this is ugly, but it works and there is a much better solution
-    #when bug http://code.opencv.org/issues/2505 is patched and sent downstream
-    x, y = 0, 0
-    maxx = -1
-    minx = -1
-    maxy = -1
-    miny = -1
-    for i in a:
-        if i[0][0] < minx or minx == -1:
-            minx = i[0][0]
-        if i[0][0] > maxx:
-            maxx = i[0][0]
-        if i[0][1] < miny or miny == -1:
-            miny = i[0][1]
-        if i[0][1] > maxy:
-            maxy = i[0][1]
-        
-        x += i[0][0]
-        y += i[0][1]
 
-    size = (maxx - minx)*(maxy - miny)
-    x = x/len(a)
-    y = y/len(a)
-    return size, x, y
+def average(points):
+    """Calculate a rough estimate of size and central point of a polygon by
+    using a bounding box.
+
+    """
+    # yeah I know this is ugly, but it works and there is a better solution
+    # when bug http://code.opencv.org/issues/2505 is patched and downstreamed
+    if not points:
+        return 0, 0, 0
+
+    x_axis = [point[0][0] for point in points]
+    y_axis = [point[0][1] for point in points]
+    size = (max(x_axis) - min(x_axis)) * (max(y_axis) - min(y_axis))
+    x_avg = sum(x_axis) / len(points)
+    y_avg = sum(y_axis) / len(points)
+    return size, x_avg, y_avg
+
 
 def process(points):
-    entries = len(points)
-    if entries == 0:
+    """Check the array of points to see if we can find an overall direction of
+    the movement. If we can print useful phrases to stdout that can be piped
+    into a control script.
+
+    """
+    if not points:
         return
     yoffset = 0.0
     xoffset = 0.0
-    for i in range(0,entries):
-        for j in range(i+1,entries):
-            yoffset += points[j][1] - points[i][1]
-            xoffset += points[j][0] - points[i][0]
-    
-    #so now we have the large vector, so lets compare the two components
-    if abs(xoffset) > 160 or abs(yoffset) > 120:
-        #print xoffset,yoffset
-        if abs(xoffset) > 100+abs(yoffset):
-            if xoffset > 0:
-                #left
-                print "prev"
-            else:
-                #right
-                print "next"
-        if abs(yoffset) > 100+abs(xoffset):
-            if yoffset > 0:
-                #down
-                print "pause"
-            else:
-                #up
-                print "pause"
+    for index, previous in enumerate(points):
+        for leading in points[index+1:]:
+            xoffset += leading[0] - previous[0]
+            yoffset += leading[1] - previous[1]
+
+    greater, lesser = sorted([abs(xoffset), abs(yoffset)])
+
+    # so now we have the large vector, so lets compare the two components
+    if abs(xoffset) > (WIDTH / 2) or abs(yoffset) > (HEIGHT / 2) and \
+            greater > (DIFFERENCE + lesser):
+        if abs(xoffset) > abs(yoffset):  # x-axis
+            if xoffset > 0:  # left
+                print("prev")
+            else:  # right
+                print("next")
+        elif abs(yoffset) > abs(xoffset):  # y-axis
+            if yoffset > 0:  # down
+                print("pause")
+            else:  # up
+                print("pause")
         sys.stdout.flush()
 
 
+def main(debug=False):
+    """The main routine that continually captures images, does preprocessing
+    and then sends them to be processed after a movement has been detected.
 
-c = cv2.VideoCapture(0)
-c.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)
-c.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
+    """
+    camera = cv2.VideoCapture(0)
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, WIDTH)
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, HEIGHT)
 
-img = cv2.cvtColor(c.read()[1], cv2.COLOR_RGB2GRAY)
+    img = cv2.cvtColor(camera.read()[1], cv2.COLOR_RGB2GRAY)
+    avg = numpy.float32(img)
+    pointlist = []
 
-avg = np.float32(img)
+    while(True):
+        # Preprocess
+        img = cv2.cvtColor(camera.read()[1], cv2.COLOR_RGB2GRAY)
+        img = cv2.medianBlur(img, 3)
+        cv2.accumulateWeighted(img, avg, 0.2)
+        img = cv2.subtract(cv2.convertScaleAbs(img), cv2.convertScaleAbs(avg))
+        _, thrsh = cv2.threshold(img, 15, 255, 0)
 
-pointlist = []
+        # Find large objects in the image that aren't in the static background
+        contours, _ = cv2.findContours(
+            thrsh,
+            cv2.RETR_LIST,
+            cv2.CHAIN_APPROX_NONE
+        )
 
-while(1):
-    img = cv2.cvtColor(c.read()[1], cv2.COLOR_RGB2GRAY)
-    
-    img = cv2.medianBlur(img, 3)
+        # If there are large objects, use the largest and add it's central
+        # point to the list of points in a motion
+        # Otherwise process the previous motion list to see if there was any
+        # discernable uni-directional motion
+        if contours:
+            largest_contour = max([average(contour) for contour in contours])
+            if largest_contour[0] > 100:
+                pointlist.append(largest_contour[1:])
+        else:
+            process(pointlist)
+            pointlist = []
 
-    cv2.accumulateWeighted(img, avg, 0.2)
-    
-    img = cv2.subtract(cv2.convertScaleAbs(img),cv2.convertScaleAbs(avg))
-    _, thrsh = cv2.threshold(img,15,255,0)
-    
-    contours, _ = cv2.findContours(thrsh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    maxarea = 0
-    realx = -1
-    realy = -1
+        if debug:
+            cv2.imshow('img', img)
+            k = cv2.waitKey(20)
 
-    for i in contours:
-        area, x, y = average(i)
-        if area > maxarea and area > 100:
-            maxarea = area
-            realx = x
-            realy = y
-    
-    if realx == -1 and realy == -1:
-        process(pointlist)
-        pointlist = []
-    else:
-        pointlist.append((realx,realy))
-    
-    #cv2.imshow('img',img)
-    #k = cv2.waitKey(20)
+            if k == 27:
+                break
 
-    #if k == 27:
-    #    break
+    if debug:
+        cv2.destroyAllWindows()
 
-#cv2.destroyAllWindows()
-c.release()
+    camera.release()
+
+
+if __name__ == '__main__':
+    main()
